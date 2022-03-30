6df84b1b80c3f77a51c2d3e317cb60a90e5b736f
Sebastian Wehrmann <sebastian@risclog.com>
2022-03-30 20:12:26 +0200

    Improve zsh integration

--- home-manager/modules/common.nix
+++ home-manager/modules/common.nix
@@ -16,6 +16,7 @@
     zsh-powerlevel10k
     zsh-syntax-highlighting
     zsh-history-substring-search
+    thefuck
     imagemagick
     gnupg
     tmux
--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -4,13 +4,23 @@
     enable = true;
     oh-my-zsh = {
       enable = true;
-      plugins = [ "git", "mercurial", "macos", "thefuck" ];
+      plugins = [ "git" "mercurial" "macos" "thefuck" ];
       theme = "robbyrussell";
+      extraConfig = ''
+        # Load seperated config files
+        for conf in "$HOME/.config/zsh/config.d/"*.zsh; do
+          source "''${conf}"
+        done
+        unset conf
+      '';
     };
-    plugins = [
-      { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
-      { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
-    ];
+    zplug = {
+      enable = true;
+      plugins = [
+        { name = "zsh-users/zsh-autosuggestions"; } # Simple plugin installation
+        { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; } # Installations with additional options. For the list of options, please refer to Zplug README.
+      ];
+    };
   };
 }
 
