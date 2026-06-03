541d2bc32c2b858b80ed7c954616fdf2f05ff570
Sebastian Wehrmann <sebastian@risclog.com>
2026-06-03 07:23:07 +0200

    Remove thefuck

--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -10,7 +10,7 @@
     };
     oh-my-zsh = {
       enable = true;
-      plugins = [ "git" "mercurial" "macos" "thefuck" ];
+      plugins = [ "git" "mercurial" "macos" ];
       theme = "robbyrussell";
       extraConfig = ''
         eval "$(zoxide init zsh)"
